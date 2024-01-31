4108d23d21e6ea24f3215267dc360d010f91716a
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-01-31 10:18:25 +0100

    Add open menu

--- ts/altmenu.ts
+++ ts/altmenu.ts
@@ -1,11 +1,13 @@
 import { ContextMenuItem } from "$types/app";
 import { EditMenu } from "./altmenu/edit";
 import { FileMenu } from "./altmenu/file";
+import { OpenMenu } from "./altmenu/open";
 import { ViewMenu } from "./altmenu/view";
 import { Runtime } from "./runtime";
 
 export const TextEditorAltMenu: (runtime: Runtime) => ContextMenuItem[] = (runtime: Runtime) => [
   FileMenu(runtime),
   EditMenu(runtime),
-  ViewMenu(runtime)
+  ViewMenu(runtime),
+  OpenMenu(runtime)
 ]
---
+++ ts/altmenu/open.ts
@@ -0,0 +1,38 @@
+import { AppsIcon } from "$ts/images/general";
+import { MarkdownMimeIcon } from "$ts/images/mime";
+import { getPartialFile } from "$ts/server/fs/file";
+import { OpenWith } from "$ts/server/fs/file/handler";
+import { openFileWithApp } from "$ts/server/fs/open";
+import { ContextMenuItem } from "$types/app";
+import { Runtime } from "../runtime";
+
+export function OpenMenu(runtime: Runtime): ContextMenuItem {
+
+  return {
+    caption: "Open",
+    subItems: [
+      {
+        caption: `Open file in...`,
+        disabled: () => !runtime.path.get(),
+        image: AppsIcon,
+        async action() {
+          const path = runtime.path.get();
+          const partial = await getPartialFile(path);
+
+          OpenWith(partial, runtime.pid, true);
+        }
+      },
+      {
+        caption: "Open as Markdown",
+        disabled: () => !runtime.path.get() || !runtime.path.get().endsWith(".md"),
+        image: MarkdownMimeIcon,
+        async action() {
+          const path = runtime.path.get();
+          const partial = await getPartialFile(path);
+
+          await openFileWithApp("MarkdownViewer", partial);
+        }
+      }
+    ]
+  }
+}
